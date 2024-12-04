69c810068b72bf7baa899a24dfc032cc75900379
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-12-04 12:22:03 -0700

    Ran cypress tests to test locally and adjusted config files for cypress to run

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'cypress';
-import viteConfig from './vite.config';
+import { defineConfig } from "cypress";
+// import the vite config file for cypress component tests
+import viteConfig from "./vite.config";
 
 export default defineConfig({
   component: {
     port: 5173,
     devServer: {
-      framework: 'react',
-      bundler: 'vite',
+      framework: "react",
+      bundler: "vite",
       viteConfig,
     },
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: "http://localhost:3001",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/_tests_/setup.ts'
+    environment: "jsdom",
+    setupFiles: "./src/_tests_/setup.ts",
   },
   server: {
-    port: 3000,
+    port: 3001,
     open: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      "/api": {
+        target: "http://localhost:3001",
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+});
